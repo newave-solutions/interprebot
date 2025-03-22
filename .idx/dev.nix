b17febd8736d6f9488393eb560e9465c7d53fc76
limones.rdz.eladio <limones.rdz.eladio@gmail.com>
2025-03-22 04:12:19 +0000

    ss  with '#' will be ignored, and an empty message aborts the commit.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,52 +1,40 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
+  # Use https://search.nixos.org/packages to  find packages
   packages = [
-      pkgs.go
-      pkgs.python311
-      pkgs.python311Packages.pip
-      pkgs.nodejs_20
-      pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
   ];
   # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "vscodevim.vim"
-    ];
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
+  env = {    
+    #TODO Get a API key from https://g.co/ai/idxGetGeminiKey 
+    GOOGLE_GENAI_API_KEY = "AIzaSyAAfcmiqwLgiAquc_TO7Wexqk5Jpq6CT3Y"; 
+  };
+  # search for the extension on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "ms-toolsai.jupyter"
+      "ms-toolsai.jupyter-keymap"
+  ];
+  idx.workspace = {
+    # Runs when a workspace is first created with this `dev.nix` file
+    onCreate = {
+      npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+      default.openFiles = [ "README.md" "static/index.html" "server.ts" ];
     };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+    # To run something each time the workspace is (re)started, use the `onStart` hook
+  };
+  # preview configuration, identical to monospace.json
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [ "npm" "run" "dev" ];
+        manager = "web";
+        env = {
+          PORT = "$PORT";
+          GENKIT_ENV = "dev";
+        };
       };
     };
   };
